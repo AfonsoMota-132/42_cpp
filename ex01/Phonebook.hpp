4bed9a46be9d875989d683d8cf6d08ae514c0244
Afonso Goncalves Mota <afogonca@c1r5s6.42porto.com>
2024-12-18 13:20:29 +0000

    res

---
+++ ex01/Phonebook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afogonca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:54:27 by afogonca          #+#    #+#             */
+/*   Updated: 2024/11/25 10:01:36 by afogonca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+class PhoneBook {
+	
+};
+
+class Contact {
+	private:
+
+	public:
+	Contact();
+	~Contact();
+	std::string	FirstName;
+	std::string	LastName;
+
+};
+#endif
