cd1e7df75e96919b2b41326b251cb139daa64ae7
Afonso Goncalves Mota <afogonca@c1r5s6.42porto.com>
2024-12-18 12:17:40 +0000

    res

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -12,17 +12,44 @@
 
 #include <iostream>
 
+std::string ft_trim(std::string str)
+{
+	size_t	start;
+	size_t	end;
+	std::string	string;
+
+	start = str.find_first_not_of(' ');
+	end = str.find_last_not_of(' ');
+	string = str.substr(start, end - start + 1);
+	return (string);
+}
+
+std::string	ft_capitalize(std::string str)
+{
+	int		len;
+	std::string	trimmed;
+	std::string	lowered;
+
+	trimmed = ft_trim(str);
+	len = trimmed.length();
+	for(int i = -1; i < len; ++i)
+		lowered += std::toupper(trimmed[i]);
+	return (lowered);
+}
+
 int	main(int ac, char **av)
 {
-	int	i;
+	std::string output;
 
 	if (ac >= 2)
 	{
-		i = 0;
-		while (++i < ac)
-			std::cout << av[i];
+		for (int i = 1; i < ac; i++)
+		{
+			output.append(ft_capitalize(av[i]) + " ");
+		}
 	}
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::cout << std::endl;
+		output.append("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	
+	std::cout << output << std::endl;
 }
