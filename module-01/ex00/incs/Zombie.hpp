a1d9211f752ef85604bc5479283ab7d4302f70bf
AfonsoMota-132 <afonsomota88@gmail.com>
2024-12-20 19:20:12 +0000

    res

---
+++ module-01/ex00/incs/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afogonca <afogonca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 16:14:40 by afogonca          #+#    #+#             */
+/*   Updated: 2024/12/20 16:20:14 by afogonca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie {
+	private:
+		std::string Name;
+	public:
+	Zombie();
+	~Zombie();
+	void announce();
+};
+
+#endif
